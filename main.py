1d81dc2707c093363d77bfaa1ff804dc418187d9
Avinash Meka <avinash.meka.92@gmail.com>
2021-06-02 22:09:33 +0530

    average and highest

--- main.py
+++ main.py
@@ -23,6 +23,14 @@ try:
 
     print(name, " has the highest salary ($", round(highval, 2), ").")
 
+    if l > 0 and row[3] == "Employee":
+        s = s + int(row[2])
+        c = c + 1
+        l = l + 1
+
+    print("The average salary is ", round(s / c, 2), " Dollars.")
+
+
 except:
     print("File not found")
     traceback.print_exc()
